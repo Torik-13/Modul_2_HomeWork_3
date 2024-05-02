7caa17acbfee24817c5cc26a7be68d1e285d26cf
Torik-13 <167818792+Torik-13@users.noreply.github.com>
2024-05-02 18:50:08 +0300

    Add files via upload

---
+++ HomeWork3.py
@@ -0,0 +1,8 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in cars:
+    print('Я езжу на автомабиле марки', i)
+
+cars_count = 0
+for j in range(2):
+    cars_count += 10
+    print(cars_count)

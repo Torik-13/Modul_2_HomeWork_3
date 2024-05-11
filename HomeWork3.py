13ad49d4cc3fca6586df60ffcbc83567809e88c9
Torik-13 <167818792+Torik-13@users.noreply.github.com>
2024-05-11 13:03:16 +0300

    Update HomeWork3.py

--- HomeWork3.py
+++ HomeWork3.py
@@ -1,8 +1,7 @@
 cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
 for i in cars:
     print('Я езжу на автомабиле марки', i)
-
-cars_count = 0
-for j in range(2):
-    cars_count += 10
-    print(cars_count)
+    if i != "BMW":
+        cars_count += 10
+        print(cars_count)
